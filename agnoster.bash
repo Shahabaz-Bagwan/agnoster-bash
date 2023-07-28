51992ba08b527e156caf536154af1a6acce06fdf
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2023-07-28 09:13:22 +0200

    add elapsed time for previous command run

--- agnoster.bash
+++ agnoster.bash
@@ -194,12 +194,42 @@ prompt_segment() {
     [[ -n $3 ]] && PR="$PR$3"
 }
 
+# Add last runtime to prompt https://stackoverflow.com/a/31694983/6699440
+function timer_now {
+  date +%s%N
+}
+
+function timer_start {
+  timer_start=${timer_start:-$(timer_now)}
+}
+
+function timer_stop {
+  local delta_us=$((($(timer_now) - $timer_start) / 1000))
+  local us=$((delta_us % 1000))
+  local ms=$(((delta_us / 1000) % 1000))
+  local s=$(((delta_us / 1000000) % 60))
+  local m=$(((delta_us / 60000000) % 60))
+  local h=$((delta_us / 3600000000))
+  # Goal: always show around 3 digits of accuracy
+  if ((h > 0)); then timer_show=${h}h${m}m
+  elif ((m > 0)); then timer_show=${m}m${s}s
+  elif ((s >= 10)); then timer_show=${s}.$((ms / 100))s
+  elif ((s > 0)); then timer_show=${s}.$(printf %03d $ms)s
+  elif ((ms >= 100)); then timer_show=${ms}ms
+  elif ((ms > 0)); then timer_show=${ms}.$((us / 100))ms
+  else timer_show=${us}us
+  fi
+  unset timer_start
+}
+
+trap 'timer_start' DEBUG
+
 # End the prompt, closing any open segments
 prompt_end() {
     if [[ -n $CURRENT_BG ]]; then
         declare -a codes=($(text_effect reset) $(fg_color $CURRENT_BG))
-        mem=`free --mega | awk 'NF>6{printf (":%2.2fGB/%2.2fGB\n", ( $3 / 1024), ($2 / 1024))}'`
-        PR="\[\033[0;34m\]╭─\[\033[0;34m\]$RIGHT_SEPARATOR$PR $(ansi codes[@])$SEGMENT_SEPARATOR \d \t $mem \n\[\033[0;34m\]╰─$LEFT_SUBSEG \[\033[1;36m\]\$\[\033[0m\]"
+        mem=`free --mega | awk 'NF>6{printf (" :%2.2fGB/%2.2fGB\n", ( $3 / 1024), ($2 / 1024))}'`
+        PR="\[\033[0;34m\]╭─\[\033[0;34m\]$RIGHT_SEPARATOR$PR $(ansi codes[@])$SEGMENT_SEPARATOR \d \t $mem 󰔟:${timer_show} \n\[\033[0;34m\]╰─$LEFT_SUBSEG \[\033[1;36m\]\$\[\033[0m\]"
     fi
     declare -a reset=($(text_effect reset))
     PR="$PR $(ansi reset[@])"
@@ -454,6 +484,7 @@ build_prompt() {
 # use that.
 
 set_bash_prompt() {
+    timer_stop
     RETVAL=$?
     PR=""
     PRIGHT=""
